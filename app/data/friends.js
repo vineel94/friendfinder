b9d1399ac9384dbd7b4eddf287e052a171754d52
Vineel <vineel@MandavaMBP.local>
2018-07-06 07:20:06 -0500

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,94 @@
+var friends = [
+    {
+      name: "Anakin",
+      photo: "http://media.comicbook.com/2017/04/hayden-christensen-anakin-skywalker-star-wars-990926.jpg",
+      scores: [
+          "1",
+          "5",
+          "3",
+          "3",
+          "4",
+          "3",
+          "1",
+          "2",
+          "1",
+          "2"
+      ]
+      
+    },
+
+    {
+        name: "Obi-Wan",
+        photo: "https://media.wired.com/photos/599623d28d00942cb02e0f1b/master/w_582,c_limit/obiwon-TA.jpg",
+        scores: [
+            "2",
+            "1",
+            "3",
+            "4",
+            "2",
+            "5",
+            "3",
+            "5",
+            "2",
+            "2"
+        ]
+        
+      },
+
+      {
+        name: "Yoda",
+        photo: "https://vignette.wikia.nocookie.net/starwars/images/d/d6/Yoda_SWSB.png/revision/latest/scale-to-width-down/500?cb=20150206140125",
+        scores: [
+            "5",
+            "1",
+            "1",
+            "5",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "1"
+        ]
+        
+      },
+
+      {
+        name: "Palpatine",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/8/84/Palpatine1.jpg/220px-Palpatine1.jpg",
+        scores: [
+            "3",
+            "4",
+            "2",
+            "5",
+            "1",
+            "2",
+            "3",
+            "5",
+            "1",
+            "5"
+        ]
+        
+      },
+
+      {
+        name: "Padme",
+        photo: "https://vignette.wikia.nocookie.net/starwars/images/b/b2/Padmegreenscrshot.jpg/revision/latest/scale-to-width-down/500?cb=20100423143631",
+        scores: [
+            "4",
+            "2",
+            "2",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "4",
+            "1"
+        ]
+        
+      },
+
+]
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var PORT = process.env.PORT || 8080;
+
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use(bodyParser.json({ type: 'application/*+json'}))
+
+app.use(bodyParser.raw({type: 'application/vnd.custom-type'}))
+
+app.use(bodyParser.text({type: 'text/html'}))
+
+require("./app/routing/api-routes.js")(app);
+require("./app/routing/html-routes.js")(app);
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT);
+});
